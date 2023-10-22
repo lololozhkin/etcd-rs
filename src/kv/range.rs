061b1cf514c0ddd45bf51f2273b2e820de7d86f3
lololozhkin <aal@darktree.com>
2023-10-22 22:34:44 +0500

    feature: added all options to range request and documented old ones

--- src/kv/range.rs
+++ src/kv/range.rs
@@ -36,19 +36,82 @@ impl RangeRequest {
         self
     }
 
+    /// Sets the revision of range request. So, this request will observe changes only
+    /// of this and previous revisions, not the latest ones.
+    /// If the revision is less than or equal to zero, the latest revision applies.
+    /// 0 by default.
     pub fn revision(mut self, revision: i64) -> Self {
         self.proto.revision = revision;
         self
     }
 
+    /// Option for specifying that the response will contain keys only, without values.
+    pub fn keys_only(mut self) -> Self {
+        self.proto.keys_only = true;
+        self
+    }
+
+    /// Option for specifying that response will contain only the amount of corresponding keys.
+    pub fn count_only(mut self) -> Self {
+        self.proto.count_only = true;
+        self
+    }
+
+    /// Sets the min mod revision of key value pairs returned in the response
+    pub fn min_mod_revision(mut self, mod_revision: i64) -> Self {
+        self.proto.min_mod_revision = mod_revision;
+        self
+    }
+
+    /// Sets the max mod revision of key value pairs returned in the response
+    pub fn max_mod_revision(mut self, mod_revision: i64) -> Self {
+        self.proto.max_mod_revision = mod_revision;
+        self
+    }
+
+    /// Sets the min create revision of key value pairs returned in the response
+    pub fn min_create_revision(mut self, create_revision: i64) -> Self {
+        self.proto.min_create_revision = create_revision;
+        self
+    }
+
+    /// Sets the max create revision of key value pairs returned in the response
+    pub fn max_create_revision(mut self, create_revision: i64) -> Self {
+        self.proto.max_create_revision = create_revision;
+        self
+    }
+
+    /// Response key value pairs will be sorted by key with provided sort order.
     pub fn sort_by_key(mut self, order: SortOrder) -> Self {
         self.proto.sort_target = etcdserverpb::range_request::SortTarget::Key as i32;
         self.proto.sort_order = order.into();
         self
     }
 
+    /// Response key value pairs will be sorted by version with provided sort order.
     pub fn sort_by_version(mut self, order: SortOrder) -> Self {
         self.proto.sort_target = etcdserverpb::range_request::SortTarget::Version as i32;
+        self.proto.sort_order = order.into();
+        self
+    }
+
+    /// Response key value pairs will be sorted by create revision with provided sort order.
+    pub fn sort_by_create_revision(mut self, order: SortOrder) -> Self {
+        self.proto.sort_target = etcdserverpb::range_request::SortTarget::Create as i32;
+        self.proto.sort_order = order.into();
+        self
+    }
+
+    /// Response key value pairs will be sorted by mod revision with provided sort order.
+    pub fn sort_by_mod_revision(mut self, order: SortOrder) -> Self {
+        self.proto.sort_target = etcdserverpb::range_request::SortTarget::Mod as i32;
+        self.proto.sort_order = order.into();
+        self
+    }
+
+    /// Response key value pairs will be sorted by value with provided sort order.
+    pub fn sort_by_value(mut self, order: SortOrder) -> Self {
+        self.proto.sort_target = etcdserverpb::range_request::SortTarget::Value as i32;
         self.proto.sort_order = order.into();
         self
     }
